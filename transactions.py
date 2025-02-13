4d843d982d541b3d34310264141fcf7af6f96995
Vamsik31 <vamsik3107@gmail.com>
2025-02-13 12:04:58 -0500

    changes

--- transactions.py
+++ transactions.py
@@ -2,51 +2,25 @@ def greet(name):
     return f"Hello, {name}!"
 
 
- 
-def create_transaction(connection, Transaction_Id, Member_id, Date,Amount,Tran_type,Desc:None):
+
+def create_transaction(connection, member_id, amount, description, transaction_type = "withdrawal"):
     cursor = connection.cursor()
     cursor.execute(f"""
-    INSERT INTO members (Transaction_Id, Member_id, Date,Amount,Tran_type,Descfname, lname, gender)
-    VALUES (%s, %s, %s,%s, %s, %s);
-    """, (Transaction_Id, Member_id, Date,Amount,Tran_type,Desc))
+    INSERT INTO fi_member_transactions (member_id, amount, transaction_type, description)
+    VALUES (%s, %s, %s, %s) RETURNING transaction_id ;
+    """, (member_id, amount, transaction_type, description))
     connection.commit()
-    #id = cursor.fetchone()[0]
-    print(f"Record inserted successfully!")
+    id = cursor.fetchone()[0]
+    print(f"Record inserted successfully!: {id}")
+    return id
 
 
-def retrieve_transactions(connection, member_id=None):
+def retrieve_transactions(connection, member_id):
     cursor = connection.cursor()
-    if(member_id is None):
-        query = "SELECT * FROM transactions;"
-    else:
-        query = f"SELECT * FROM transactions where id = {member_id};"
-        #query = "SELECT * FROM members where id = " + member_id
+    query = f"SELECT * FROM fi_member_transactions where member_id = {member_id} order by transaction_date desc;"
     cursor.execute(query)
     rows = cursor.fetchall()
     connection.commit()
     for row in rows:
         print(row)
-
-
-def update_transactions(connection, Transaction_Id, Member_id, Date,Amount,Tran_type,Desc):
-    cursor = connection.cursor()
-
-    cursor.execute("""
-    UPDATE members
-    SET first_name = %s, last_name = %s
-        WHERE id = %s;
-    """, (Transaction_Id, Member_id, Date,Amount,Tran_type,Desc))
-    #connection.commit()
-    print("Record updated successfully!")
-    print("updated data")
-    retrieve_transactions(connection, Transaction_Id)
-
-
-def delete_transactions(connection, member_id):
-    cursor = connection.cursor()
-    cursor.execute(f"""
-    DELETE FROM members
-    WHERE id = {member_id};
-    """)
-    connection.commit()
-    print("Record deleted successfully!")
+    return rows
